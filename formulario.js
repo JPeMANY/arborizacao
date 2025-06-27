f2941cf343914f404d1fa77144ff09f29669a38f
JPeMANY <116897201+JPeMANY@users.noreply.github.com>
2025-06-27 09:50:56 -0300

    Add files via upload

---
+++ formulario.js
@@ -0,0 +1,20 @@
+document.getElementById('btnEnviar').addEventListener('click', function(event) {
+    event.preventDefault(); // Previne o envio do formulário
+
+    // Obtém os valores dos campos
+    const nome = document.getElementById('nome').value;
+    const cpf = document.getElementById('cpf').value;
+
+    if (nome.value === null && cpf.value === null) {
+
+    } else {
+        
+    }
+
+    // Salva os valores no LocalStorage
+    localStorage.setItem('nome', nome);
+    localStorage.setItem('cpf', cpf);
+
+    // Redireciona para a página /inicio.html
+    window.location.href = '/inicio.html';
+});
