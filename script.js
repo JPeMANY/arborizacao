c7d02bd20799d3124b8e13a60d80e09f18931901
JPeMANY <116897201+JPeMANY@users.noreply.github.com>
2025-07-04 11:37:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,34 @@ h1Nome.textContent = nome;
 spanCpf.textContent = cpf;
 
 const botaoNovaPropriedade = document.querySelector('.adcionarNovaPropriedade');
+const botaoFecharPopup = document.querySelector('.icon_fechar_popup');
+const popup_adicionar_propriedade = document.getElementById('popup_adicionar_propriedade');
+
+botaoNovaPropriedade.addEventListener('click', () => {
+    abrirFecharPopup();
+})
+
+botaoFecharPopup.addEventListener('click', () => {
+    abrirFecharPopup();
+})
+
+function abrirFecharPopup() {
+    popup_adicionar_propriedade.classList.toggle('none');
+}
+
+
+const botoesExcluir = document.querySelectorAll('.opcoes .excluir');
+const botoesEditar = document.querySelectorAll('.opcoes .editar');
+
+botoesEditar.forEach(botao => {
+    botao.addEventListener('click', () => {
+        abrirFecharPopup();
+    })
+})
+
+const cartoesPropriedades = document.querySelectorAll('#propriedades .caixa');
+botoesExcluir.forEach(botao => {
+    botao.addEventListener('click', (event, index) => {
+        console.log(index)
+    })
+})
